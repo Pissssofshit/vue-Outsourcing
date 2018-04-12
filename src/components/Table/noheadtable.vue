6f88ba1a04e5001ff860affc8adf968b297207a2
Pissssofshit <1412143367@qq.com>
2018-04-12 17:09:32 +0800

    412

---
+++ src/components/Table/noheadtable.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-table
+    :show-header=false
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column
+      label="日期"
+     >
+      <template slot-scope="scope">
+        <slot name="firstslot"></slot>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="日期"
+      >
+      <template slot-scope="scope">
+        <slot name="firstslot"></slot>
+      </template>
+    </el-table-column>
+    </el-table>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    },
+    methods: {
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
+    }
+  }
+</script>
---
+++ src/pages/Task/taskpage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div >
+    <basiclayout :title=title :data=data></basiclayout>
+  </div>
+</template>
+<script>
+import Basiclayout from '../../components/layout/basiclayout.vue'
+export default {
+  components:{
+      Basiclayout
+  },
+    data(){
+        return{
+            title: '账号信息',
+      data: [
+        [{ type: 1, menuname: '个人信息' ,url:'个人信息'},
+          { type: 2, menuname: '账户信息', url: 'main' },
+          { type: 3, menuname: '个人文件', url: '个人文件' }
+        ]
+      ]
+        }
+    }
+}
+</script>
